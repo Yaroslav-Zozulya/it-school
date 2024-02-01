a0f95da130e30a893b017f5108e2102182f4c752
Yaroslav-Zozulya <my.dev.stories@gmail.com>
2024-02-01 17:30:01 +0300

    update

--- front/src/components/common/PageCoursesList/CourseList/CourseList.jsx
+++ front/src/components/common/PageCoursesList/CourseList/CourseList.jsx
@@ -12,9 +12,8 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "/src/components/common/styledButtonsForSlider.scss";
 import "./styledProgressBar.scss";
+import { endpoints } from "@/services/serverAPI";
 import s from "./courseList.module.scss";
-
-const BASE_URL = "http://localhost:1337";
 
 export default function CourseList() {
   const [courses, setCourses] = useState([]);
@@ -55,7 +54,7 @@ export default function CourseList() {
             <SwiperSlide key={id} className={s.cource_item}>
               <div className={s.course_item_thumb}>
                 <Image
-                  src={`${BASE_URL}${previewImg.data.attributes.url}`}
+                  src={`${endpoints.BASE_URL}${previewImg.data.attributes.url}`}
                   width={200}
                   height={200}
                   alt="image course info"
